64e2c49d1196b4f4ffbf99129f2188958f513940
Dean Doyle <deandoyle1502@gmail.com>
2024-11-27 20:48:01 +0000

    Added file type validation and logging valid file to DynaamoDb

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -8,9 +8,10 @@ import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
-
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,7 +21,14 @@ export class EDAAppStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
+
       
+    });
+    // Table
+
+    const imageTable = new dynamodb.Table(this, "ImageTable", {
+      partitionKey: { name: "fileName", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
       // Integration infrastructure
 
@@ -56,6 +64,16 @@ export class EDAAppStack extends cdk.Stack {
     entry: `${__dirname}/../lambdas/mailer.ts`,
   });
 
+  const logImageFn = new lambdanode.NodejsFunction(this, "LogImageFn", {
+    runtime: lambda.Runtime.NODEJS_18_X,
+    entry: `${__dirname}/../lambdas/logImage.ts`,
+    timeout: cdk.Duration.seconds(15),
+    memorySize: 128,
+    environment: {
+      DYNAMODB_TABLE_NAME: imageTable.tableName,
+    },
+  });
+
   // S3 --> SQS
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
@@ -79,13 +97,22 @@ newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
     maxBatchingWindow: cdk.Duration.seconds(5),
   }); 
 
+  const logImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(5),
+  });
+
   processImageFn.addEventSource(newImageEventSource);
 
   mailerFn.addEventSource(newImageMailEventSource);
 
+  logImageFn.addEventSource(logImageEventSource);
+
   // Permissions
 
   imagesBucket.grantRead(processImageFn);
+
+  imageTable.grantReadWriteData(logImageFn)
 
   mailerFn.addToRolePolicy(
     new iam.PolicyStatement({
